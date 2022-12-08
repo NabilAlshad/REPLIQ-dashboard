3f7b69e4fc8fbd5c6717a611df31c10d6ed342d7
nabil alshad <nabilalshad01@gmail.com>
2022-12-08 22:53:17 +0600

    text color changed

--- layouts/Common/LayoutOne.jsx
+++ layouts/Common/LayoutOne.jsx
@@ -4,7 +4,7 @@ export const LayoutOne = ({left , right}) => {
   return (
     <div className='flex flex-row justify-between mx-auto my-3 mb-3 w-[50%]'>
         <p className='font-bold font-mono text-xl'>{left}</p>
-        <p className='text-gray-200 font-mono text-lg'>{right}</p>
+        <p className='text-gray-400 font-mono text-lg'>{right}</p>
     </div>
   )
 }
